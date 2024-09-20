2e48694548bb7cda58c8921c77dd668f5117c11f
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2024-09-20 07:13:46 -0700

    ElementScreen constructor no longer has default texture size

--- src/main/java/slimeknights/tconstruct/library/client/Icons.java
+++ src/main/java/slimeknights/tconstruct/library/client/Icons.java
@@ -10,19 +10,19 @@ public interface Icons {
 
   //ElementScreen ANVIL = new ElementScreen(ICONS, 18 * 3, 0, 18, 18, 256, 256);
   ElementScreen PATTERN = new ElementScreen(ICONS, 0, 18 * 12, 18, 18, 256, 256);
-  //ElementScreen SHARD = new ElementScreen(ICONS, 18, 18 * 12, 18, 18);
-  //ElementScreen BLOCK = new ElementScreen(ICONS, 18 * 2, 18 * 12, 18, 18);
-  //ElementScreen PICKAXE = new ElementScreen(ICONS, 0, 18 * 13, 18, 18);
-  //ElementScreen DUST = new ElementScreen(ICONS, 18, 18 * 13, 18, 18);
-  //ElementScreen LAPIS = new ElementScreen(ICONS, 18 * 2, 18 * 13, 18, 18);
-  ElementScreen INGOT = new ElementScreen(ICONS, 18 * 3, 18 * 13, 18, 18);
-  //ElementScreen GEM = new ElementScreen(ICONS, 18 * 4, 18 * 13, 18, 18);
-  //ElementScreen QUARTZ = new ElementScreen(ICONS, 18 * 5, 18 * 13, 18, 18);
-  ElementScreen BUTTON = new ElementScreen(ICONS, 180, 216, 18, 18);
-  ElementScreen BUTTON_HOVERED = new ElementScreen(ICONS, 180 + 18 * 2, 216, 18, 18);
-  ElementScreen BUTTON_PRESSED = new ElementScreen(ICONS, 180 - 18 * 2, 216, 18, 18);
+  //ElementScreen SHARD = new ElementScreen(ICONS, 18, 18 * 12, 18, 18, 256, 256);
+  //ElementScreen BLOCK = new ElementScreen(ICONS, 18 * 2, 18 * 12, 18, 18, 256, 256);
+  //ElementScreen PICKAXE = new ElementScreen(ICONS, 0, 18 * 13, 18, 18, 256, 256);
+  //ElementScreen DUST = new ElementScreen(ICONS, 18, 18 * 13, 18, 18, 256, 256);
+  //ElementScreen LAPIS = new ElementScreen(ICONS, 18 * 2, 18 * 13, 18, 18, 256, 256);
+  ElementScreen INGOT = new ElementScreen(ICONS, 18 * 3, 18 * 13, 18, 18, 256, 256);
+  //ElementScreen GEM = new ElementScreen(ICONS, 18 * 4, 18 * 13, 18, 18, 256, 256);
+  //ElementScreen QUARTZ = new ElementScreen(ICONS, 18 * 5, 18 * 13, 18, 18, 256, 256);
+  ElementScreen BUTTON = new ElementScreen(ICONS, 180, 216, 18, 18, 256, 256);
+  ElementScreen BUTTON_HOVERED = new ElementScreen(ICONS, 180 + 18 * 2, 216, 18, 18, 256, 256);
+  ElementScreen BUTTON_PRESSED = new ElementScreen(ICONS, 180 - 18 * 2, 216, 18, 18, 256, 256);
 
-  ElementScreen PIGGYBACK_1 = new ElementScreen(ICONS, 18 * 13, 0, 18, 18);
-  ElementScreen PIGGYBACK_2 = new ElementScreen(ICONS, 18 * 13, 18, 18, 18);
-  ElementScreen PIGGYBACK_3 = new ElementScreen(ICONS, 18 * 13, 18 * 2, 18, 18);
+  ElementScreen PIGGYBACK_1 = new ElementScreen(ICONS, 18 * 13, 0, 18, 18, 256, 256);
+  ElementScreen PIGGYBACK_2 = new ElementScreen(ICONS, 18 * 13, 18, 18, 18, 256, 256);
+  ElementScreen PIGGYBACK_3 = new ElementScreen(ICONS, 18 * 13, 18 * 2, 18, 18, 256, 256);
 }
